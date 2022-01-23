6ea5773b13ab0d941d36e8b201403334938ee101
Amman Abua <ammanabua@gmail.com>
2022-01-23 18:12:55 +0000

    add endpoints

--- webapi/src/index.ts
+++ webapi/src/index.ts
@@ -3,6 +3,18 @@ import * as express from 'express';
 const app: any = express();
 const router = express.Router();
 
+let quoteStore = new Map<string, string>();
+
+router.post('/v1/quote', async (req, res) => {
+  await quoteStore.set(req.body.quote, req.body.quote)
+  res.send(`Added ${req.body.quote}`)
+});
+
+router.get('/v1/quote-list', async (req, res) => {
+  let quoteList = Array.from(await quoteStore.entries()).map(x => x[1])
+  res.send(quoteList);
+});
+
 router.get('/', async (req, res) => {
   res.send("Hello World!");
 });
